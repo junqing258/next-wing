aee08e0a905a8dfecb1b01fbad0d2fa3d8ddc4ff
Junqing <1551486328@qq.com>
2019-11-06 21:50:35 +0800

    plugins

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
-const withSass = require('@zeit/next-sass');
-const withProgressBar = require('next-progressbar');
+const withPlugins = require('next-compose-plugins');
+// const images = require('next-images');
+const sass = require('@zeit/next-sass');
+const typescript = require('@zeit/next-typescript');
+const progressBar = require('next-progressbar');
+// const offline = require('next-offline');
+// const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next-server/constants');
 
-module.exports = withProgressBar(
-    withSass({
-        cssModules: true
-    })
-);
+//https://github.com/zeit/next-plugins
 
-
+module.exports = withPlugins([
+  [
+    sass,
+    {
+      cssModules: true,
+      cssLoaderOptions: {
+        localIdentName: '[path]___[local]___[hash:base64:5]',
+      },
+    },
+  ],
+  typescript,
+  // offline,
+  progressBar,
+]);
 
 // module.exports = withCSS(withImages({
 //     inlineImageLimit: 16384,
